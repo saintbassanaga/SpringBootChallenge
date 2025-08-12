36d5195eda7e9e7ef4dbe804753019fcd92808b0
saintbassanaga <saintbassanaga01@gmail.com>
2025-08-12 14:15:20 +0100

    refactor: changing organisation package naming systems

--- src/main/java/tech/saintbassanaga/springbootchallenge/SpringBootChallengeApplication.java
+++ src/main/java/tech/saintbassanaga/springbootchallenge/SpringBootChallengeApplication.java
@@ -1,4 +1,4 @@
-package tech.bassanaga.springbootchallenge;
+package tech.saintbassanaga.springbootchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
